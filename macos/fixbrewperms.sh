05a93087a4e07426bc0ebd78930133341c13eb83
Andrew Gray <adsgray@gmail.com>
2017-01-27 11:03:33 -0800

    old brew permisisons fix that I forgot to commit

--- macos/fixbrewperms.sh
+++ macos/fixbrewperms.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+sudo chown andrew:admin /usr/local/
 sudo chown -R andrew:admin /usr/local/share
 sudo chown -R andrew:admin /usr/local/bin
+sudo chown -R andrew:admin /usr/local/Library
