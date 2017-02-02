f485e2fafb64fd9b14d8ddc5228fa8ab032f3d22
Andrew Gray <adsgray@gmail.com>
2017-02-02 09:51:10 -0800

    so cool, a shell script that sends e-mail

---
+++ bash/emailarticles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# requires:
+# brew install sendemail
+
+. ~/notes/secrets
+
+msgfile=`mktemp`
+articledir=~/notes/article
+today=`date "+%Y-%m-%d"`
+zipdir="$articledir/zip"
+articlezipfile=articles-$today.zip
+
+cd $zipdir
+
+if [ -f $articlezipfile ]; then
+    subject="articles $today"
+    echo "Articles for ${today}:" > $msgfile
+    articlecount.sh list >> $msgfile
+    sendemail -f $gmailaddress -t $gmailaddress -a $articlezipfile -s smtp.gmail.com -o tls=yes -xu $gmailusername -xp $gapppassword -u $subject -o message-file=$msgfile
+    rm $msgfile
+else
+    echo "$articlezipfile not found in $zipdir"
+fi
