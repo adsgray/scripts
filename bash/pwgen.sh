d44053c7e03ec177311ec93e1c4abcb209763938
Andrew Gray <andrew.gray@hootsuite.com>
2019-12-05 09:12:23 -0800

    pwgen outputs a single string

--- bash/pwgen.sh
+++ bash/pwgen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-for f in $*; do randomword $f|cut -b 1-5|tr '[sStTaAeEhHoOlL]' '[557744%%##0011]'; done
+for f in $*; do randomword $f|cut -b 1-5|tr '[sStTaAeEhHoOlL]' '[557744%%##0011]'|tr -d '\n'; done
+echo ""
