77fd54fb5254ac692904692eefb399b3f1bf53db
Andrew Gray <andrew.gray@hootsuite.com>
2019-12-11 08:47:01 -0800

    pwgen takes input

--- bash/pwgen.sh
+++ bash/pwgen.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-for f in $*; do randomword $f|cut -b 1-5|tr '[sStTaAeEhHoOlL]' '[557744%%##0011]'|tr -d '\n'; done
+read -p "input: " inp
+
+for f in $inp; do randomword $f|cut -b 1-5|tr '[sStTaAeEhHoOlL]' '[557744%%##0011]'|tr -d '\n'; done
 echo ""
